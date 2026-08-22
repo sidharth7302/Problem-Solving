ff4bdbad00508cb741fad5fd140c88a9f7419790
sidharth7302 <149807192+sidharth7302@users.noreply.github.com>
2026-08-22 10:58:25 +0530

    ✅ Two Sum | Easy | Python | Runtime: 1 ms (61.33%) | Memory: 20.7 MB | 2026-08-22

--- leetcode/easy/0001-two-sum/solution.py
+++ leetcode/easy/0001-two-sum/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-       prevmap={}
-       for i,n in enumerate(nums):
-                      
-            n2=target-n
-            if n2 in prevmap:
-                 return [prevmap[n2],i]
-            prevmap[n]=i    
+        prevmap={}
+        for i,n in enumerate(nums):
+            diff = target-n
+            if diff in prevmap:
+                return [prevmap[diff],i]
+            prevmap[n]=i   
+        return     
